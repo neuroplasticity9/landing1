6dcad56d59ffebbf0956d01d6d71bd1eeadaf22d
COTOK <vuquyettam@sohagame.vn>
2023-01-09 16:17:17 +0700

    News project landing

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  // transpilePackages: ['react-confirm-alert', 'react-date-picker', 'react-konva'],
+  eslint: {
+    dirs: ['.']
+  },
+  reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
+  swcMinify: true,
+  experimental: {
+    // Required:
+    // appDir: true
+    // runtime: 'experimental-edge'
+    // esmExternals: 'loose',
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+  ],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+  theme: {
+    extend: {
+      // https://vercel.com/design/color
+    },
+  },
+  // plugins: [require('@tailwindcss/forms')],
+};
